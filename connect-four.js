d90c0f0ddf11f76e4ceb765e4b26db967926d603
Erick Bravo <erickbrvo@gmail.com>
2020-09-10 15:13:08 -0700

    stuck at end of 2nd part

--- connect-four.js
+++ connect-four.js
@@ -5,32 +5,32 @@ window.addEventListener("DOMContentLoaded", () => {
     let game = undefined
 
     // class Game {
-    //     //manage player names as well as creating the name of the specific game.
-    // }
+        //     //manage player names as well as creating the name of the specific game.
+        // }
 
 
-    const boardholder = document.getElementById("board-holder");
-    const newGame = document.getElementById("new-game");
+        const boardholder = document.getElementById("board-holder");
+        const newGame = document.getElementById("new-game");
 
-    const player1Input = document.getElementById("player-1-name");
-    const player2Input = document.getElementById("player-2-name");
-    const newGameButton = document.getElementById("new-game");
+        const player1Input = document.getElementById("player-1-name");
+        const player2Input = document.getElementById("player-2-name");
+        const newGameButton = document.getElementById("new-game");
 
-    const ifForInputs = () => {
-        let playerOne = player1Input.value;
-        let playerTwo = player2Input.value;
+        const ifForInputs = () => {
+            let playerOne = player1Input.value;
+            let playerTwo = player2Input.value;
         if((playerOne.length !== 0) && (playerTwo.length !== 0)) {
 
             newGameButton.disabled = false;
         }
-     }
+    }
 
-     player1Input.addEventListener('keyup', e => {
-         ifForInputs();
-     })
+    player1Input.addEventListener('keyup', e => {
+        ifForInputs();
+    })
 
 
-     player2Input.addEventListener('keyup', e => {
+    player2Input.addEventListener('keyup', e => {
         ifForInputs();
     })
 
@@ -43,6 +43,7 @@ window.addEventListener("DOMContentLoaded", () => {
 
     });
 
+    const clickTargets = document.getElementById("click-targets");
     function updateUI(){
         const boardHolder = document.getElementById('board-holder');
         const gameName = document.getElementById("game-name")
@@ -51,8 +52,24 @@ window.addEventListener("DOMContentLoaded", () => {
         }else{
             boardHolder.classList.remove('is-invisible');
             gameName.innerHTML = game.getName();
+            const currentPlayer = game.currentPlayer;
+            // clickTargets.addEventListener("click", () => {
+                if(currentPlayer === 1) {
+                    clickTargets.classList.add("black")
+                    clickTargets.classList.remove("red")
+                } else {
+                    clickTargets.classList.add("red")
+                    clickTargets.classList.remove("black")
+                }
+            // })
         }
     }
+
+    clickTargets.addEventListener("click", (e) => {
+        const target = e.target.id;
+        game.playInColumn()
+        updateUI()
+    })
 
 
 
